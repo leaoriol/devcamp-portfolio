f7ee452ebbe3b22933d6fc9b1e128e984a0953a5
Lea Oriol <lea.oriol.bc@gmail.com>
2017-11-12 14:26:06 -0800

    implement edit functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -21,6 +21,25 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def edit
+    # new - create something from scratch
+    # edit - needs to fetch the actual record
+    @portfolio_item = Portfolio.find(params[:id]) 
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio was updated' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+
   private
     def portfolio_params
       params.require(:portfolio).permit(:title, :subtitle, :body)
