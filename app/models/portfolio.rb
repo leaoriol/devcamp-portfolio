3f504d7c05dc5cb65fcb4eda251292e4dc449f5c
Lea Oriol <lea.oriol.bc@gmail.com>
2017-11-13 20:38:10 -0800

    implement custom scopes for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,9 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  def self.react
+    where(subtitle: 'react')
+  end
+
+  scope :ruby_on_rails, -> { where(subtitle: 'ruby on rails') }
 end
