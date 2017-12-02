4150406264a568581fd2b8b9dce022cb25ac757b
Lea Oriol <lea.oriol.bc@gmail.com>
2017-12-02 11:30:10 -0800

    Implemented authoritzation engine for portfolio item and blog

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,8 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
   layout "blogs"
+  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+
 
   # GET /blogs
   # GET /blogs.json
